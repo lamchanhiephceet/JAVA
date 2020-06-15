cd5a1a4573e96a26b42fb22b9757f4856cb04c68
lamchanhiephceet <lamchanhiephceet@gmail.com>
2020-06-15 16:02:05 +0700

    *.java

---
+++ TH_HE_CAC_DOI_TUONG_HINH_HOC/src/RectangleTest.java
@@ -0,0 +1,9 @@
+public class RectangleTest {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.3, 3.8, "orange", true);
+        System.out.println(rectangle);
+    }
+}
