6493543577b3935887f3bac7cda51625e20a991d
lamchanhiephceet <lamchanhiephceet@gmail.com>
2020-06-17 17:16:57 +0700

    *.java

---
+++ TH_refactoring_method/src/CylinderTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CylinderTest {
+
+    @Test
+    @DisplayName("Testing getVolume(0, 0)")
+    public void testGetVolume0And0() {
+        int radius = 0;
+        int height = 0;
+        double expected = 0;
+
+        double result = Cylinder.getVolume(radius, height);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Testing getVolume(1, 2)")
+    public void testGetVolume1And2() {
+        int radius = 1;
+        int height = 2;
+        double expected = 18.84955592153876;
+
+        double result = Cylinder.getVolume(radius, height);
+        assertEquals(expected, result);
+    }
+}
