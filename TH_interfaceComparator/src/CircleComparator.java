5c76a395e925f97540df20f488b7f7e588c11158
lamchanhiephceet <lamchanhiephceet@gmail.com>
2020-06-17 10:56:14 +0700

    *.java

--- TH_interfaceComparator/src/CircleComparator.java
+++ TH_interfaceComparator/src/CircleComparator.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
 
-public class CircleComparator {
+import java.util.Comparator;
+public class CircleComparator implements Comparator<Circle> {
+
+    @Override
+    public int compare(Circle c1, Circle c2) {
+        if (c1.getRadius() > c2.getRadius()) return 1;
+        else if (c1.getRadius() < c2.getRadius()) return -1;
+        else return 0;
+    }
+
 }
