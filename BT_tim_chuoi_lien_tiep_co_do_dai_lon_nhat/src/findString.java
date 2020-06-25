b0a622012bc04e2433036936c2846582d27333e4
lamchanhiephceet <lamchanhiephceet@gmail.com>
2020-06-25 16:24:10 +0700

    *.java

---
+++ BT_tim_chuoi_lien_tiep_co_do_dai_lon_nhat/src/findString.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class findString {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        LinkedList<Character> max = new LinkedList<>();
+        LinkedList<Character> list = new LinkedList<>();
+
+        System.out.println("Enter a string: ");
+        String string = input.nextLine();
+
+        for (int i = 0; i < string.length(); i++) {
+            if (list.size() > 1 && string.charAt(i) <= list.getLast() && list.contains(string.charAt(i))) {
+                list.clear();
+            }
+            list.add(string.charAt(i));
+            if (list.size() > max.size()) {
+                max.clear();
+                max.addAll(list);
+            }
+        }
+
+        for (Character ch : max) {
+            System.out.print(ch);
+
+        }
+        System.out.println();
+    }
+}
