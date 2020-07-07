4478843659b7533f14a04f2a3065aa16cc37ecf4
lamchanhiephceet <lamchanhiephceet@gmail.com>
2020-07-07 16:49:47 +0700

    *

---
+++ TH_TrienKhaiProxy/src/Calculator.java
@@ -0,0 +1,6 @@
+public interface Calculator {
+    double add(double first, double second);
+    double sub(double first, double second);
+    double mul(double first, double second);
+    double div(double first, double second);
+}
---
+++ TH_TrienKhaiProxy/src/MathCalculator.java
@@ -0,0 +1,21 @@
+public class MathCalculator implements Calculator {
+    @Override
+    public double add(double first, double second) {
+        return first + second;
+    }
+
+    @Override
+    public double sub(double first, double second) {
+        return first - second;
+    }
+
+    @Override
+    public double mul(double first, double second) {
+        return first * second;
+    }
+
+    @Override
+    public double div(double first, double second) {
+        return first / second;
+    }
+}
